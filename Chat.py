d8a4d89ea429e586179dff3b1463be1b7241bbdf
Icon-1999 <armonjlee99@gmail.com>
2023-10-22 17:06:52 -0700

    UI fix

--- Chat.py
+++ Chat.py
@@ -2,12 +2,12 @@ import socket
 import os
 
 def help():
-    print("1. help\n" + 
-          "2. myip\n" + 
-          "3. myport\n" + 
-          "4. connect <destination> <port no>\n" + 
-          "5. list\n" +
-          "6. terminate <connection id.>\n" +
+    print("1. help\n"
+          "2. myip\n"
+          "3. myport\n"
+          "4. connect <destination> <port no>\n"
+          "5. list\n"
+          "6. terminate <connection id.>\n"
           "7. send <connection id.> <message>\n"
           "8. exit\n")
     
@@ -40,30 +40,37 @@ def exit():
     
 def UI():
     help()
-    UserChoice = input("\n")
+    UserChoice = ''
+
     while(UserChoice != "exit"):
+
+        UserChoice = input("Select from the options above: ")
+
         match UserChoice:
             case "help":
                 help()
             case "myip":
                 myip()
+                help()
             case "myport":
                 myport()
+                help()
             case "connect":
                 connect(destination, port)
+                help()
             case "list":
                 list()
+                help()
             case "terminate":
                 terminate(connection_id)
+                help()
             case "send":
                 send(connection_id, message)
+                help()
             case "exit":
                 exit()
             case _:
                 print("invalid input please choose from the choices above\n")
                 help()
-        
-        help()
-        UserChoice = input("\n")
 
 UI()
