8e2e2a0dd2883b76f10dfcedbb6cee14b8dacf1b
Armon Lee <armonlee99@gmail.com>
2023-11-08 14:25:51 -0800

    rcv msg

--- SocketExample2.py
+++ SocketExample2.py
@@ -6,7 +6,7 @@ hostname = socket.gethostname()
 # get the IP address
 my_ip_address = socket.gethostbyname(hostname)
 
-print(my_ip_address)
+print('localhost')
 
 HOST = my_ip_address #any working ip
 PORT = 4445
@@ -26,3 +26,16 @@ s.listen(10)
 conn, addr = s.accept()
 
 print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+# receive message string from
+# server, at a time 1024 B
+msg = s.recv(1024)
+  
+# repeat as long as message
+# string are not empty
+while msg:
+    print('Received:' + msg.decode())
+    msg = s.recv(1024)
+ 
+# disconnect the client
+s.close()
