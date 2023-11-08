901329e7481a215656c4aa5a2c5d24932691cf96
Armon Lee <armonlee99@gmail.com>
2023-11-08 14:53:48 -0800

    fix

--- SocketExample2.py
+++ SocketExample2.py
@@ -4,13 +4,13 @@ import sys
 # get the hostname
 hostname = socket.gethostname()
 # get the IP address
-my_ip_address = socket.gethostbyname(hostname)
+#127.0.0.1 = socket.gethostbyname(hostname)
 
-print('127.0.0.1')
+print('localhost')
 
-HOST = my_ip_address #any working ip
+HOST = '127.0.0.1'  # any working IP
 PORT = 4445
- 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
@@ -18,7 +18,7 @@ try:
 except socket.error as msg:
     print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
     sys.exit()
-	
+
 print('Socket bind complete')
 
 s.listen(10)
@@ -29,13 +29,13 @@ print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
 # receive message string from
 # server, at a time 1024 B
-msg = s.recv(1024)
-  
+msg = conn.recv(1024)
+
 # repeat as long as message
-# string are not empty
+# string is not empty
 while msg:
     print('Received:' + msg.decode())
-    msg = s.recv(1024)
+    msg = conn.recv(1024)
 
 # disconnect the client
 s.close()
